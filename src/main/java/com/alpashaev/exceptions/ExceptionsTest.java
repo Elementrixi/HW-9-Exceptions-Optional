7d92d7236e1b0dfd02eaf4c6c2bd2cf3dc978fee
Elementrixi <67051385+Elementrixi@users.noreply.github.com>
2020-07-15 21:23:08 +0300

    reworked code

--- src/main/java/com/alpashaev/exceptions/ExceptionsTest.java
+++ src/main/java/com/alpashaev/exceptions/ExceptionsTest.java
@@ -4,43 +4,47 @@ import java.io.IOException;
 
 public class ExceptionsTest {
     private static final boolean enableA = false;
-    private static final boolean enableB = false;
 
-
-    public ExceptionsTest() {
+    public static void exceptionsTest() {
+        exceptionA();
+        exceptionB(true);
+        nullPointerException(enableA, false);
+        ioException();
     }
 
     public static void exceptionA() {
-        if (ExceptionsTest.enableA) {
+        if (!ExceptionsTest.enableA) {
             try {
                 throw new ExceptionA();
             } catch (ExceptionA exceptionA) {
-                System.out.println("Exception typo A caught ");
+                System.out.println("Exception type A caught ");
             }
         }
     }
 
-    public static void exceptionB() {
-        if (ExceptionsTest.enableB) {
+    public static void exceptionB(boolean enableB) {
+        if (enableB) {
             try {
                 throw new ExceptionB();
             } catch (ExceptionB exceptionB) {
-                System.out.println("Exception typo A caught ");
+                System.out.println("Exception type B caught ");
             }
         }
     }
-    public static void nullPointerException(boolean enableA, boolean enableB){
+
+    public static void nullPointerException(boolean enableA, boolean enableB) {
         try {
             throw new NullPointerException();
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("NullPointerException caught");
         }
     }
-    public static void ioException(){
+
+    public static void ioException() {
         try {
             throw new IOException();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (NullPointerException | IOException e) {
+            System.out.println("IOException caught");
         }
     }
 }
